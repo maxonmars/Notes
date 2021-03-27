e3877764f137c0f8d7f5af46a6c68480b277c184
Sergey-lang <sergei.kuharyonok@yandex.by>
2021-03-27 12:18:03 +0300

    delete redux tools

--- src/App/store.ts
+++ src/App/store.ts
@@ -11,8 +11,6 @@ export const rootReducer = combineReducers({
 type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
 export type InferActionsTypes<T extends { [key: string]: (...args: any) => any }> = ReturnType<PropertiesTypes<T>>
 
-//@ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export type AppStateType = ReturnType<typeof rootReducer>
-export let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
+export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
